c488cbe0d6d5be3487516f4c38dd6e2156ae7b26
FKAV64 <fomavalerio@gmail.com>
2026-04-13 15:11:29 +0300

    Adım 4: RBAC middleware - TeacherOnly guard

--- 08-week-8/golearn/middleware/rbac.go
+++ 08-week-8/golearn/middleware/rbac.go
@@ -1,6 +1,21 @@
 package middleware
-import "github.com/gin-gonic/gin"
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func TeacherOnly() gin.HandlerFunc {
-	return func(c *gin.Context) { c.Next() }
+	return func(c *gin.Context) {
+		role := c.GetString("role")
+		if role != "teacher" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "Bu işlem sadece öğretmenler için",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
